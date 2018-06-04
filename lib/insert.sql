2d34ac2e2754516e10741b3bdb8990d16b4cfbd2
Grace Shih <vocalise@gmail.com>
2018-06-04 03:40:22 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,13 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate", 5, "M", "blue", "happy", )
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate", 5, "M", "blue", "happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Rowdy", 10, "F", "pink", "playful", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Tabitha", 8, "F", "blue", "happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Sergeant Brown", 3, "M", "green", "mad", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Melissa", 2, "M", "blue", "happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Grinch", 8, "M", "black", "grumpy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Wendy", 1, "F", "orange", "sad", 1);

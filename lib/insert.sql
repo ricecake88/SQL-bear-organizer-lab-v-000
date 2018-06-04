35df8c325f31cd6637577040e986ee21ca0bba0f
Grace Shih <vocalise@gmail.com>
2018-06-04 03:56:42 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Mr. Chocolate", 5, "M", "blue", "happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Rowdy", 10, "F", "pink", "playful", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Tabitha", 8, "F", "blue", "happy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Sergeant Brown", 3, "M", "green", "mad", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Melissa", 2, "M", "blue", "goofy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Grinch", 8, "M", "black", "grumpy", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES("Wendy", 1, "F", "orange", "sad", 1);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES(NULL, NULL, "M", "red", "angry", 0);

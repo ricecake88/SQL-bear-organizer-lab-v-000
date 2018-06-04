30437a0393f9352b1f578fb8ed08ef9be020a864
Grace Shih <vocalise@gmail.com>
2018-06-04 03:47:10 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,15 +7,15 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC"
+  "SELECT name, age FROM bears WHERE alive = 1 ORDER BY age ASC;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, MAX(age) FROM bears"
+  "SELECT name, MAX(age) FROM bears;"
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, MIN(age) FROM bears;"
 end
 
 def selects_most_prominent_color_and_returns_with_count
